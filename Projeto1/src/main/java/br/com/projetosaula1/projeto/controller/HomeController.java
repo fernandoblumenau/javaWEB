12f591e143eabfbfa3836a49d52c16341f3941f8
Fernando Rodrigues <fernando_criciuma@hotmail.com>
2022-03-24 22:04:43 -0300

    First Commit

---
+++ Projeto1/src/main/java/br/com/projetosaula1/projeto/controller/HomeController.java
@@ -0,0 +1,25 @@
+package br.com.projetosaula1.projeto.controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "HomeController", urlPatterns = "/")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public HomeController() {
+    	super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
